e201dad752e79b0c88c0a42f92915473385a7249
Alex <alex.ruperto@javierlabs.com>
2024-03-11 18:53:46 -0400

    Started working on website!

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <div>
+      <img src="@/assets/javierlabs logo transparent.png" alt="logo" class="logo">
+      <h1>Javier Labs</h1>
+    </div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
   <router-view/>
 </template>
 
 <style>
 #app {
+  background-color:#332E3C;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #D8D4F2;
+  min-height: 100vh;
+}
+
+.logo {
+  width: 5%;
+  height: auto;
 }
 
 nav {
@@ -27,4 +40,16 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+router-view {
+  display: block;
+  margin: 0;
+  padding: 0;
+}
+html, body {
+  height: 100%;
+  margin: 0;
+}
 </style>
+
+
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img src="@/assets/1.jpg" alt="Description" class="responsive-image">
+  </div>
+  <div>
+    <img src="@/assets/2.jpg" alt="Description" class="responsive-image">
+  </div>
+  <div>
+    <img src="@/assets/3.jpg" alt="Description" class="responsive-image">
+  </div>
+  <div>
+    <img src="@/assets/4.jpg" alt="Description" class="responsive-image">
+  </div>
+  <div>
+    <img src="@/assets/5.jpg" alt="Description" class="responsive-image">
+  </div>
+  <div>
+    <img src="@/assets/6.jpg" alt="Description" class="responsive-image">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  // If you have props, define them here
+};
+</script>
+
+<style>
+.image-container {
+  max-width: 100% 
+}
+
+.responsive-image {
+  max-width: 70%;
+  height:auto
+}
+</style>
