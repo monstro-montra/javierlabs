16482223791e066440998e00a8b9bc8bf1c2260f
Alex Ruperto <alex.ruperto@vanderlande.com>
2024-03-20 10:10:03 -0400

    Added title to homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,31 @@
 <template>
+  <div class = "Title">
+    <h1>Javier Labs</h1>
+  </div>
   <div class = "Logo">
     <img src="@/assets/javierlabs logo transparent.png" alt="Logo" />
   </div>
-  <div class = "Title">
-    <h1> Software Developer, ML Engineer, and Photographer </h1>
+  <div class = "Description">
+    <h2> Software Developer, ML Engineer, and Photographer </h2>
     <h3> I develop software focused on machine learning and shoot photography. </h3>
   </div>
 </template>
 
 <style>
+
+  .Title {
+    text-align:left;
+    margin: 0;
+    margin-left: 2%
+  }
+
   .Logo img {
     height: auto;
     max-width: 100px;
-    padding-top: 10%;
+    margin-top: 5%;
   }
 
-  .Title {
-    padding-top:5%
+  .Description {
+    margin-top:5%
   }
 </style>
