2fd1482f06b96df7baf69395f8dd69f496734e71
Alex Ruperto <alex.ruperto@vanderlande.com>
2024-03-20 21:39:37 -0400

    Test

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="about">
-      <h1>This is the page where I post all my projects.</h1>
+      <h1>This is the page where I post all of my projects.</h1>
     </div>
   </template>
   
